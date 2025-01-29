f88aafe78a80cfd1eaab58f45631f3951d1afbae
Marilyn Haber <habermarilyn2002@gmail.com>
2025-01-29 23:00:37 +0200

    Add Cors to API

--- OliverCuisine.API/Program.cs
+++ OliverCuisine.API/Program.cs
@@ -13,14 +13,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IStoreEntities>(provider => provider.GetRequiredService<StoreDbContext>());
 builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
